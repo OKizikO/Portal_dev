6bca7013ba122ee4bde207f67066766451d80c52
Darth Vader <Chris.Geese@iCloud.com>
2023-12-27 23:28:39 -0500

    sqlite in progress

--- data.py
+++ data.py
@@ -1,6 +1,19 @@
 import sqlite3
 
-connection = sqlite3.connect('test.db')
-cursor = connection.cursor()
+conn = sqlite3.connect('test.db')
+cursor = conn.cursor()
 
 
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS daily_metrics (
+    date TEXT PRIMARY KEY,
+    alerts REAL,
+    district_rank TEXT,
+    market_rank TEXT,
+    company_rank TEXT,
+    ucr REAL
+)
+''')
+
+conn.commit()
+conn.close()
