8a991a24352ea23baf082758737d59da1dded5fd
Darth Vader <Chris.Geese@iCloud.com>
2023-12-28 13:34:00 -0500

    edits

--- data.py
+++ data.py
@@ -4,16 +4,3 @@ conn = sqlite3.connect('test.db')
 cursor = conn.cursor()
 
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS daily_metrics (
-    date TEXT PRIMARY KEY,
-    alerts REAL,
-    district_rank TEXT,
-    market_rank TEXT,
-    company_rank TEXT,
-    ucr REAL
-)
-''')
-
-conn.commit()
-conn.close()
